22272e8d5ece8d41bb23359e36a43ffceb1d91b6
sattaya037 <sattaya.ra@ics-th.com>
2019-08-15 17:09:03 +0700

    make it better

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+const { WebhookClient } = require('dialogflow-fulfillment')
+app.get('/', express.json(),  (req, res) => res.send('online'))
+
+app.post('/webhook', (req, res) => {
+  //Create an instance
+  const agent = new WebhookClient({ request: req, response: res })
+  //Function Location
+  function BMI(agent) {
+    let weight = request.body.queryResult.parameters.weight;
+    let height = request.body.queryResult.parameters.height / 100;
+    let bmi = (weight / (height * height)).toFixed(2);
+    agent.add(bmi);
+
+  }
+  let intentMap = new Map();
+  intentMap.set('BMI - custom - yes', BMI);
+  agent.handleRequest(intentMap);
+});
+
 app.listen(port)
