e4f50273f872f3ee98788959db96244a9aa0fac9
sattaya037 <sattaya.ra@ics-th.com>
2019-08-15 18:04:15 +0700

    make it better

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ app.post('/webhook', (req, res) => {
   const agent = new WebhookClient({ request: req, response: res })
   //Function Location
   function BMI(agent) {
-    let weight = request.body.queryResult.parameters.weight;
-    let height = request.body.queryResult.parameters.height / 100;
-    let bmi = (weight / (height * height)).toFixed(2);
-    agent.add(bmi);
+    agent.add("Hello Heroku");
 
   }
   let intentMap = new Map();
