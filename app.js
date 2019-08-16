3a1a6fb2c81d0b58c8cd9b895cb0b09a2d956c55
sattaya037 <sattaya.ra@ics-th.com>
2019-08-16 13:24:49 +0700

    make it better

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 4000;
 const {
   WebhookClient
 } = require('dialogflow-fulfillment');
+const functions = require('firebase-functions');
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
@@ -22,51 +23,52 @@ app.post('/webhook',express.json(), (req, res) => {
   console.log('POST: /');
   console.log('Body: ',req.body);
   console.log('headers: ',req.headers);
-
-
-  //Create an instance
-
-    const agent = new WebhookClient({
-      request: req,
-      response: res
-    });
+  exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+      //Create an instance
+      const agent = new WebhookClient({
+        request: req,
+        response: res
+      });
+      
     
+      //Test get value of WebhookClient
+      console.log('agentVersion: ' + agent.agentVersion);
+      console.log('intent: ' + agent.intent);
+      console.log('locale: ' + agent.locale);
+      console.log('query: ', agent.query);
+      console.log('session: ', agent.session);
+    
+      //Function Location
+      function welcome(agent) {
+        agent.add("Welcome to my agent!");
+      }
+     
+      function fallback(agent) {
+        agent.add("I didn't understand");
+        agent.add("I'm sorry, can you try again?");
+      }
+      
+      function BMI(agent) {
+        let weight = agent.parameters.weight;
+        console.log(weight);
+        // const weight = agent.parameters['weight'];
+        agent.add(weight);
   
-    //Test get value of WebhookClient
-    console.log('agentVersion: ' + agent.agentVersion);
-    console.log('intent: ' + agent.intent);
-    console.log('locale: ' + agent.locale);
-    console.log('query: ', agent.query);
-    console.log('session: ', agent.session);
-  
-    //Function Location
-    function welcome(agent) {
-      agent.add("Welcome to my agent!");
-    }
+        // let height = request.body.queryResult.parameters.height / 100;
+        // let bmi = (weight / (height * height)).toFixed(2);
+    
+      }
+    
+      // Run the proper function handler based on the matched Dialogflow intent name
+      let intentMap = new Map();
+      intentMap.set('Default Welcome Intent', welcome);
+      intentMap.set('Default Fallback Intent', fallback);
+      intentMap.set('BMI - custom - yes', BMI);
+      agent.handleRequest(intentMap);
    
-    function fallback(agent) {
-      agent.add("I didn't understand");
-      agent.add("I'm sorry, can you try again?");
-    }
-    
-    function BMI(agent) {
-      let weight = agent.parameters.weight;
-      console.log(weight);
-      // const weight = agent.parameters['weight'];
-      agent.add(weight);
+  });
 
-      // let height = request.body.queryResult.parameters.height / 100;
-      // let bmi = (weight / (height * height)).toFixed(2);
-  
-    }
-  
-    // Run the proper function handler based on the matched Dialogflow intent name
-    let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-    intentMap.set('BMI - custom - yes', BMI);
-    agent.handleRequest(intentMap);
- 
+
 });
 
 app.listen(port, () => {
