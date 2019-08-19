833183b474befcd0a98065d7f90d208371312b58
sattaya037 <sattaya.ra@ics-th.com>
2019-08-19 11:26:47 +0700

    make it better

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/webhook',express.json(), (req, res) => {
         console.log('sap:'+sapRespond);
         for (let i = 0; i < sapRespond.d.results.length; i++) {
          }
-         var a =sapRespond.d.results.Firstname;
+         var a =sapRespond.d.results;
       console.log(a)
       agent.add("SAP"+a);
 
