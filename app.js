7867e9b9cd02e445b483c4b2f6bbf98c1e886441
sattaya037 <sattaya.ra@ics-th.com>
2019-08-19 17:42:19 +0700

    make it better

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/webhook',express.json(), (req, res) => {
         for (let i = 0; i < sapRespond.d.results.length; i++) {
               var name = sapRespond.d.results[i].Firstname;
               var lastname = sapRespond.d.results[i].Lastname;
-              agent.add([i]+" "+name+" "+lastname);         
+              agent.add(name+" "+lastname);         
         } 
       }
       function SAPInfo(agent) {
