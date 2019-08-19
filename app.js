f7236e99f37c496cfa9ac0866643ad31b9b90352
sattaya037 <sattaya.ra@ics-th.com>
2019-08-19 12:36:00 +0700

    make it better

--- app.js
+++ app.js
@@ -48,6 +48,8 @@ app.post('/webhook',express.json(), (req, res) => {
       function BMI(agent) {
         // let weight = agent.parameters.weight;
         let weight = req.body.queryResult.parameters.weight;
+        console.log(weight);
+
         let height = req.body.queryResult.parameters.height;
         let BMI = weight/(height/100*height/100) ;
 
