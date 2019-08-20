8429f015459a302f1ee87f7d20b5df31131115b2
sattaya037 <sattaya.ra@ics-th.com>
2019-08-20 12:23:27 +0700

    make it better

--- app.js
+++ app.js
@@ -59,13 +59,7 @@ app.post('/webhook',express.json(), (req, res) => {
         for (let i = 0; i < sapRespond.d.results.length; i++) {
               var name = sapRespond.d.results[i].Firstname;
               var lastname = sapRespond.d.results[i].Lastname;
-              var payloadJson ={
-                "type": "text",
-                "text": sapRespond.d.results[i].Firstname
-              };
-              var payload = new Payload(`LINE`, payloadJson, { sendAsMessage: true });
-
-              agent.add(payload);  
+              agent.add(name+" "+lastname);  
         } 
      
       }
