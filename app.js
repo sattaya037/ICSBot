7f51b2a050e2c5d9406f55191eb71fa7f03175f3
sattaya037 <sattaya.ra@ics-th.com>
2019-08-20 17:13:35 +0700

    make it better

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post('/webhook',express.json(), (req, res) => {
 
       function SAPInfo(agent) {
         let fName =  req.body.queryResult.parameters.Object;
-        let firstname =  req.body.queryResult.parameters.given-name;
+        let firstname =  req.body.queryResult.parameters.person.name;
 
         const nameCapitalized = fName.charAt(0).toUpperCase() + fName.slice(1)
         // let name =  req.body.queryResult.parameters.person.name;
