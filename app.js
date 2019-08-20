9887538acdba4258a15bd0617ae6432a6ac7763a
sattaya037 <sattaya.ra@ics-th.com>
2019-08-20 17:33:31 +0700

    make it better

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.post('/webhook',express.json(), (req, res) => {
       function SAPInfo(agent) {
         let fName =  req.body.queryResult.parameters.Object;
         let firstname =  req.body.queryResult.parameters.EmployeeName;
+        let P =  req.body.queryResult.parameters.person.name;
 
         const nameCapitalized = fName.charAt(0).toUpperCase() + fName.slice(1)
         // let name =  req.body.queryResult.parameters.person.name;
@@ -148,7 +149,7 @@ app.post('/webhook',express.json(), (req, res) => {
           //       };
                 // let payload = new Payload(`LINE`, payloadJson, { sendAsMessage: true });
 
-                  agent.add(nameCapitalized+" "+firstname);         
+                  agent.add(nameCapitalized+" "+firstname+""+P);         
               //      break; 
               //  }   
         // } 
