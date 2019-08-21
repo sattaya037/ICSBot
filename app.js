c0fa5276c733da3cd4202c8c16e698fcb42ee366
sattaya037 <sattaya.ra@ics-th.com>
2019-08-21 14:49:50 +0700

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
 const port = process.env.PORT || 4000;
 
  // Import the appropriate class
@@ -36,38 +35,35 @@ app.post('/webhook',express.json(), (req, res) => {
         },
       });
       var sapRespond = JSON.parse(odata.getBody());
-
-      
-    
       //Test get value of WebhookClient
       // console.log('agentVersion: ' + agent.agentVersion);
       // console.log('intent: ' + agent.intent);
       // console.log('locale: ' + agent.locale);
       // console.log('query: ', agent.query);
       // console.log('session: ', agent.session);
-    
       //Function Location
       function BMI(agent) {
         let weight = req.body.queryResult.parameters.weight;
         let height = req.body.queryResult.parameters.height;
         let BMI = weight/(height/100*height/100) ;  
         agent.add("ํBMI:"+BMI);
-
       }
 
       function listEmployee(agent) {
         for (let i = 0; i < sapRespond.d.results.length; i++) {
               var name = sapRespond.d.results[i].Firstname;
               var lastname = sapRespond.d.results[i].Lastname;
-              agent.add(name+" "+lastname);  
+              const payloadJson = {
+                "type": "text",
+                "text": name
+              };
+              let payload = new Payload(`LINE`, payloadJson, { sendAsMessage: true });
+
+              agent.add(payload);  
         } 
-     
       }
 
-   
-
       function Info(agent) {
-
         var UserSay =agent.query;
         var wording =UserSay.slice(0, 2);
         var name = UserSay.substr(3);
