74068bf1d8e9699ed14c9a7749cb6fea416cd419
sattaya037 <sattaya.ra@ics-th.com>
2019-08-21 11:32:37 +0700

    make it better

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.post('/webhook',express.json(), (req, res) => {
         // let fName =  req.body.queryResult.parameters.Object;
         // let firstname =  req.body.queryResult.parameters.EmployeeName;
         // let condition =  req.body.queryResult.parameters.Object;
-        console.log('intent: ' + agent.intent);
+        console.log('query: ', agent.query);
         // const nameCapitalized = fName.charAt(0).toUpperCase() + fName.slice(1)
         // let name =  req.body.queryResult.parameters.person.name;
 
