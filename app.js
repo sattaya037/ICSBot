d4d0f30b4bc958ff8ceaed5a3e6c722ba0a1ec64
sattaya037 <sattaya.ra@ics-th.com>
2019-08-22 17:35:26 +0700

    make it better

--- app.js
+++ app.js
@@ -22,9 +22,6 @@ app.post('/webhook',express.json(), (req, res) => {
   console.log('POST: /');
   console.log('Body: ',req.body);
   console.log('headers: ',req.headers);
-  let msg = req.body.events[0].message.text
-  var a =msg;
-  console.log(a);
       //Create an instance
       const agent = new WebhookClient({
         request: req,
