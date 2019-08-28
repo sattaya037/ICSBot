12fbd61ef94991be57d751350c48d4409c37b3ec
sattaya037 <sattaya.ra@ics-th.com>
2019-08-28 16:01:24 +0700

    make it better

--- app.js
+++ app.js
@@ -157,6 +157,78 @@ app.post('/webhook', (req, res) => {
         var name = UserSay.substr(3);
         var nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1)
         var Firstname = '',Lastname='',Nickname='',Tel='',Email='',Birthdate='',Position='',Line='';
+        var payloadJson = {
+            "type": "flex",
+            "altText": "Flex Message",
+            "contents": {
+              "type": "bubble",
+              "direction": "ltr",
+              "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "SAP",
+                    "align": "center"
+                  }
+                ]
+              },
+              "hero": {
+                "type": "image",
+                "url": "https://ics-bot-bot.herokuapp.com/image",
+                "size": "5xl",
+                "aspectRatio": "1.91:1",
+                "aspectMode": "fit"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": Firstname+"  "+Lastname+"("+Nickname+")",
+                    "align": "center",
+                    "weight": "bold",
+                    "size": "lg"
+                  },
+                  {
+                    "type": "separator",
+                    "margin": "lg"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "spacer"
+                      },
+                      {
+                        "type": "text",
+                        "text": "Tel:"+" "+Tel
+                      },
+                      {
+                        "type": "text",
+                        "text": "Email:"+" "+Email
+                      },
+                      {
+                        "type": "text",
+                        "text": "Birthdate:"+" "+Birthdate
+                      },
+                      {
+                        "type": "text",
+                        "text": "Position:"+" "+Position
+                      },
+                      {
+                        "type": "text",
+                        "text": "Line:"+" "+Line
+                      }
+                    ]
+                  },
+                ]
+              }
+            }
+          };
         if(wording == "fn"){
           for (let i = 0; i < sapRespond.d.results.length; i++) {
             if(sapRespond.d.results[i].Firstname == nameCapitalized){
@@ -168,80 +240,6 @@ app.post('/webhook', (req, res) => {
               Birthdate +=sapRespond.d.results[i].Birthdate;
               Position +=sapRespond.d.results[i].Position;
               Line +=sapRespond.d.results[i].Line;
-
-
-              const payloadJson = {
-                "type": "flex",
-                "altText": "Flex Message",
-                "contents": {
-                  "type": "bubble",
-                  "direction": "ltr",
-                  "header": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "SAP",
-                        "align": "center"
-                      }
-                    ]
-                  },
-                  "hero": {
-                    "type": "image",
-                    "url": "https://ics-bot-bot.herokuapp.com/image",
-                    "size": "5xl",
-                    "aspectRatio": "1.91:1",
-                    "aspectMode": "fit"
-                  },
-                  "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": Firstname+"  "+Lastname+"("+Nickname+")",
-                        "align": "center",
-                        "weight": "bold",
-                        "size": "lg"
-                      },
-                      {
-                        "type": "separator",
-                        "margin": "lg"
-                      },
-                      {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [
-                          {
-                            "type": "spacer"
-                          },
-                          {
-                            "type": "text",
-                            "text": "Tel:"+" "+Tel
-                          },
-                          {
-                            "type": "text",
-                            "text": "Email:"+" "+Email
-                          },
-                          {
-                            "type": "text",
-                            "text": "Birthdate:"+" "+Birthdate
-                          },
-                          {
-                            "type": "text",
-                            "text": "Position:"+" "+Position
-                          },
-                          {
-                            "type": "text",
-                            "text": "Line:"+" "+Line
-                          }
-                        ]
-                      },
-                    ]
-                  }
-                }
-              };
               let payload = new Payload(`LINE`, payloadJson, { sendAsMessage: true });
               agent.add(payload);  
             }
