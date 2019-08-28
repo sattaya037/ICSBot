d2f62b2451142f221169b597356ef00e127edbeb
sattaya037 <sattaya.ra@ics-th.com>
2019-08-28 16:53:38 +0700

    make it better

--- app.js
+++ app.js
@@ -21,9 +21,8 @@ app.use('/image', express.static('image/ICS-Logo.png'))
 app.post('/webhook', (req, res) => {
   res.sendStatus(200)
   console.log(req.body.events[0].message);
-
-  console.log('POST: /');
-  // console.log('Body: ',req.body.originalDetectIntentRequest.payload);
+// console.log('POST: /');
+console.log('Body: ',req.body.originalDetectIntentRequest.payload);
 // console.log('headers: ',req.headers);
       const agent = new WebhookClient({
         request: req,
